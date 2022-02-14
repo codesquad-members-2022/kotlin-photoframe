13d0b4919acf1522b8b2937a6c43cc37bdd41cb4
hanchang97 <nimok97@gmail.com>
2022-02-15 03:50:56 +0900

    feat : textView 속성 변경 Button 구현 버튼 클릭 시 텍스트 뷰의 내용, 글자 색상, 배경색 변경되도록 구현

--- app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
+++ app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
@@ -1,15 +1,43 @@
 package com.codesquad.kotlinphotoframe
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    var change = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Log.d("MainActivity", "onCreate")
+
+        val tv1 = findViewById<TextView>(R.id.tv1)
+        tv1.text = "Han의 사진 액자"
+
+        val btn_change_tv1 = findViewById<Button>(R.id.btn_change_tv1)
+        btn_change_tv1.setOnClickListener {
+            if(change){
+                tv1.apply {
+                    text = "CHANGE CLICKED"
+                    setTextColor(ContextCompat.getColor(context, R.color.myGreen))
+                    setBackgroundResource(R.color.myGray)
+                }
+            }
+            else{
+                tv1.apply {
+                    text = "Han의 사진 액자"
+                    setTextColor(ContextCompat.getColor(context, R.color.black))
+                    setBackgroundColor(ContextCompat.getColor(context, R.color.white))
+                }
+            }
+            change = !change
+        }
+
     }
 
     override fun onStart() {
