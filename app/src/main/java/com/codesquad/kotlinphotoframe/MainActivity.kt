eb3941227ef45e2c0ba53a8b22d551a4bca1d9a9
liyusang1 <liyusang1@naver.com>
2022-02-14 22:47:25 +0900

    Step3 추가

--- app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
+++ app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.util.TypedValue
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,9 +21,13 @@ class MainActivity : AppCompatActivity() {
         var mainText = findViewById<TextView>(R.id.textView)
         mainText.setText("Andrew의 사진 액자")
         mainText.setTextColor(Color.MAGENTA)
-        mainText.setTextSize(30f)
+        mainText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30f)
         mainText.setBackgroundColor(Color.YELLOW)
 
-        Log.d("MainActivity", "onCreate")
+    }
+
+    fun viewMessage(view: View) {
+        Snackbar.make(view, "사진을 불러옵니다.", Snackbar.LENGTH_LONG)
+            .show();
     }
 }
