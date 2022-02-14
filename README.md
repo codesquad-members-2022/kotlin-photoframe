f3cb50c63a75915e2074e3ab0b193df7f3caa7cd
banjjak2 <interr0bang@naver.com>
2022-02-14 16:56:47 +0900

    TextView 구현하기 작업 진행

--- README.md
+++ README.md
@@ -10,3 +10,20 @@
 
 <img width="1178" alt="로그기록" src="https://user-images.githubusercontent.com/29175138/153791685-2585205f-bdf6-48c8-961f-837d7c4c216c.png">
 
+---
+
+### TextView 구현하기
+
+- [x] activity_main.xml에 TextView를 추가한다.
+  - [x] Design editor의 기능을 모두 활용해본다.
+    - text 내용, 글자색 변경 테스트
+
+- [x] MainActivity에서 TextView에 대한 참조를 얻어 text를 변경한다.
+  - [x] findViewById를 활용한다.
+  - [x] onCreate 함수에서 text를 변경한다.
+  - [x] TextView의 글자색, 배경색, 글자 크기를 변경해본다.
+
+![TextView 구현하기](https://user-images.githubusercontent.com/29175138/153821908-12d4c352-d076-42b5-aca2-4b8e5afa44a0.png)
+
+---
+
--- app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
+++ app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.codesquad.kotlinphotoframe
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.TypedValue
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +13,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.d("MainActivity", "onCreate")
+
+        val tvExplain: TextView = findViewById(R.id.tv_explain)
+        tvExplain.text = "wooki의 사진 액자^^"
+        tvExplain.setTextColor(Color.RED)
+        tvExplain.setBackgroundColor(Color.YELLOW)
+        tvExplain.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24F)
     }
 }
