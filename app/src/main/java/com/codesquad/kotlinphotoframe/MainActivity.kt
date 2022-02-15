13c88cf6fcad7ec897764cf7ceed33e7cc52ae8b
ese111 <feung94@gmail.com>
2022-02-15 20:03:55 +0900

    스위치로 다크모드
    다크모드 변경 스위치 생성과 다크모드

--- app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
+++ app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
@@ -9,10 +9,9 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.*
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.switchmaterial.SwitchMaterial
 import com.google.android.material.snackbar.Snackbar
 
 
@@ -25,10 +24,16 @@ class MainActivity : AppCompatActivity() {
         val name = "Funny"
         text.text = "${name}의 사진 액자"
         text.setTextColor(Color.parseColor("#3d00e0"))
-        val back: LinearLayout = findViewById(R.id.back)
-        back.setBackgroundColor(Color.parseColor("#ffffff"))
         val button: Button = findViewById(R.id.button)
         val contextView = findViewById<View>(R.id.context)
+        val switch: SwitchMaterial = findViewById(R.id.darkMode)
+        switch.text = "dark"
+        switch.setOnCheckedChangeListener { buttonView, isChecked ->
+            when(isChecked){
+                true -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                else -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
         if (intent.getStringExtra("msg") != null){
             Snackbar.make(contextView, intent.getStringExtra("msg")!!, Snackbar.LENGTH_SHORT).show()
         }
