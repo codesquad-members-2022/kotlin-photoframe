1cc2420ff4da67456bf72bbbb159b9bc7e5ecefd
name <junseoks9306@gmail.com>
2022-02-15 19:56:41 +0900

    add button and snackbar respectively

--- app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
+++ app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
@@ -3,7 +3,9 @@ package com.codesquad.kotlinphotoframe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +14,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_photo_frame)
         Log.d("Main", "onCreate")
 
-        val t = findViewById<TextView>(R.id.photo_frame_header2)
-        t.text = "My photoFrame"
+        val button: Button = findViewById(R.id.add_photo_button)
 
+        button.setOnClickListener{
+            Snackbar.make(
+                findViewById(R.id.custom_layout),
+                "사진 불러오는중",
+                Snackbar.LENGTH_SHORT
+            ).show()
+        }
     }
 }
