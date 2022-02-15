52b919ae3776f01b0a7077d74077821f118dc8fd
hanchang97 <nimok97@gmail.com>
2022-02-15 19:54:46 +0900

    feat : 액티비티 전환 기능 구현 두 번째 액티비티 생성 두 번째 액티비티에서 버튼을 눌러 메인 액티비티로 돌아오도록 구현 이 과정에서 문자열 데이터를 전달하고 메인 액티비티에서 해당 데이터를 받을 수 있게 구현

--- app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
+++ app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
@@ -1,32 +1,38 @@
 package com.codesquad.kotlinphotoframe
 
+import android.content.Intent
 import android.graphics.Color
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     var change = true
 
-    lateinit var tv1 : TextView
-    lateinit var btn_change_tv1 : Button
-    lateinit var btn_add_photo : Button
+    private lateinit var tv1 : TextView
+    private lateinit var btn_change_tv1 : Button
+    private lateinit var btn_add_photo : Button
+    private lateinit var btn_goto_second_activity : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("MainActivity", "onCreate")
+        Log.d("logTest", "$this - onCreate")
 
         tv1 = findViewById<TextView>(R.id.tv1)
         tv1.text = "Han의 사진 액자"
 
         setTextViewChangeButton()
         setAddPhotoButton()
+        setGotoSecondActivityButton()
 
     }
 
@@ -61,34 +67,49 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun setGotoSecondActivityButton(){
+        val getActivityResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if(it.resultCode == 1004){
+                var data = it.data?.getStringExtra("data")
+                Toast.makeText(this, "사진을 불러왔습니다\n data : $data", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btn_goto_second_activity = findViewById(R.id.btn_goto_second_activity)
+        btn_goto_second_activity.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            getActivityResult.launch(intent)
+        }
+    }
+
     override fun onResume() {
         super.onResume()
-        Log.d("MainActivity", "onResume")
+        Log.d("logTest", "$this - onResume")
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d("MainActivity", "onStart")
+        Log.d("logTest", "$this - onStart")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.d("MainActivity", "onRestart")
+        Log.d("logTest", "$this - onRestart")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d("MainActivity", "onPause")
+        Log.d("logTest", "$this - onPause")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d("MainActivity", "onStop")
+        Log.d("logTest", "$this - onStop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d("MainActivity", "onDestroy")
+        Log.d("logTest", "$this - onDestroy")
     }
 
 }
---
+++ app/src/main/java/com/codesquad/kotlinphotoframe/SecondActivity.kt
@@ -0,0 +1,69 @@
+package com.codesquad.kotlinphotoframe
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.codesquad.kotlinphotoframe.databinding.ActivitySecondBinding
+
+class SecondActivity: AppCompatActivity() {
+
+    private lateinit var binding : ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        Log.d("logTest", "$this - onCreate")
+
+        setCloseBtn()
+    }
+
+    fun setCloseBtn(){
+        binding.btnClose.setOnClickListener {
+
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("data", "데이터 전달 성공")
+            setResult(1004, intent)
+            finish()
+
+            /*
+            setResult(1004)
+            finish()
+            */
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("logTest", "$this - onResume")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("logTest", "$this - onStart")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("logTest", "$this - onRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("logTest", "$this - onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("logTest", "$this - onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("logTest", "$this - onDestroy")
+    }
+}
