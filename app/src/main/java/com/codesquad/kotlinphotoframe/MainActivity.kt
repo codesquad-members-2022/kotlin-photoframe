4e0553630ff3d6adbb15d63489bc856eba76f3af
name <junseoks9306@gmail.com>
2022-02-15 12:50:45 +0900

    change text by onCreate Class

--- app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
+++ app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
@@ -2,10 +2,16 @@ package com.codesquad.kotlinphotoframe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "onCreate")
+
+        val t = findViewById<TextView>(R.id.main_start_text)
+        t.text = "실험1"
     }
 }
