57295f357a5b3a2778e02e705958cc33b33502d2
hwj <jminie@naver.com>
2022-02-16 17:12:04 +0900

    연속해서 클릭 시 사진이 랜덤으로 바뀌도록 onClick 메서드 추가

--- app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
+++ app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
@@ -7,13 +7,14 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.TypedValue
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import java.lang.Exception
 import kotlin.random.Random
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,24 +24,29 @@ class MainActivity : AppCompatActivity() {
         myTextView.setTextColor(Color.BLUE) // 텍스트 색깔이나 사이즈를 xml 이 아니라 틀린 코드에서 동적으로 처리하는 이유?
         myTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30f)
 
-        val imageView = findViewById<ImageView>(R.id.main_image)
-        val images = listOf("01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
-            "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22")
-        val randomIdx = Random.nextInt(images.size)
-        val image = images[randomIdx]
-
         val button = findViewById<Button>(R.id.main_button)
         button.text = "사진 추가"
         button.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22f)
-        button.setOnClickListener {
-            try {
-                val assetManager = resources.assets
-                val inputStream = assetManager.open("Demo Images/$image.jpg")
-                val bitmap: Bitmap = BitmapFactory.decodeStream(inputStream)
-                imageView.setImageBitmap(bitmap)
-                inputStream.close()
-            } catch (e: Exception) {
-                e.message
+        button.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.main_button -> {
+                val imageView = findViewById<ImageView>(R.id.main_image)
+                val images = listOf("01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
+                    "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22")
+                val randomIdx = Random.nextInt(images.size)
+                val image = images[randomIdx]
+                try {
+                    val assetManager = resources.assets
+                    val inputStream = assetManager.open("Demo Images/$image.jpg")
+                    val bitmap: Bitmap = BitmapFactory.decodeStream(inputStream)
+                    imageView.setImageBitmap(bitmap)
+                } catch (e: Exception) {
+                    e.message
+                }
             }
         }
     }
