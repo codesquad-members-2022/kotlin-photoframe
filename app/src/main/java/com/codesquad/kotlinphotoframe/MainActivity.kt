b8ef551a121a277877bdf29e45cf5587e2dd1287
hwj <jminie@naver.com>
2022-02-16 16:35:24 +0900

    버튼 클릭시 랜덤 이미지 표시 구현

--- app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
+++ app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.codesquad.kotlinphotoframe
 
-import android.app.Activity
-import android.content.Intent
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,10 +10,8 @@ import android.util.TypedValue
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.constraintlayout.widget.ConstraintLayout
-import com.google.android.material.snackbar.Snackbar
+import java.lang.Exception
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,12 +23,25 @@ class MainActivity : AppCompatActivity() {
         myTextView.setTextColor(Color.BLUE) // 텍스트 색깔이나 사이즈를 xml 이 아니라 틀린 코드에서 동적으로 처리하는 이유?
         myTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30f)
 
+        val imageView = findViewById<ImageView>(R.id.main_image)
+        val images = listOf("01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
+            "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22")
+        val randomIdx = Random.nextInt(images.size)
+        val image = images[randomIdx]
+
         val button = findViewById<Button>(R.id.main_button)
         button.text = "사진 추가"
         button.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22f)
         button.setOnClickListener {
-
+            try {
+                val assetManager = resources.assets
+                val inputStream = assetManager.open("Demo Images/$image.jpg")
+                val bitmap: Bitmap = BitmapFactory.decodeStream(inputStream)
+                imageView.setImageBitmap(bitmap)
+                inputStream.close()
+            } catch (e: Exception) {
+                e.message
+            }
         }
-
     }
 }
