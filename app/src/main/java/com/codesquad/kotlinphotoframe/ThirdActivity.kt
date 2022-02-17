fc4ff27591dcfef1851c830a985a7b23aa385c32
hanchang97 <nimok97@gmail.com>
2022-02-18 01:07:59 +0900

    feat : xml 코드로 ConstraintLayout에 View 배치하기 구현 버튼 클릭 시 1~22 사이의 랜덤 정수를 문자열로 반환하는 함수 구현 위 함수를 사용하여 assets 폴더의 jpg 파일을 가져와 이미지뷰에 표시

--- app/src/main/java/com/codesquad/kotlinphotoframe/ThirdActivity.kt
+++ app/src/main/java/com/codesquad/kotlinphotoframe/ThirdActivity.kt
@@ -1,5 +1,7 @@
 package com.codesquad.kotlinphotoframe
 
+import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.codesquad.kotlinphotoframe.databinding.ActivityThirdBinding
@@ -14,12 +16,26 @@ class ThirdActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-
+        setNextBtn()
     }
 
     fun setNextBtn(){
         binding.btnNext.setOnClickListener {
-            
+            val inputStream = assets.open("missionpicture/${getRandomNumber()}.jpg")
+            /*
+            val drawable = Drawable.createFromStream(inputStream, null)
+            binding.ivPicture.setImageDrawable(drawable)
+            */
+
+            val bitmap = BitmapFactory.decodeStream(inputStream)
+            binding.ivPicture.setImageBitmap(bitmap)
+
+            // drawable, bitmap 모두 같은 결과
         }
+    }
+
+    fun getRandomNumber(): String{
+        val num = (1..22).random()
+        return if(num <= 9) "0$num" else num.toString()
     }
 }
