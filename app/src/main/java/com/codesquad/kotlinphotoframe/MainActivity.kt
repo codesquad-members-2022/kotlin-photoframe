458aadd1126af4097c52d44e22834a6ab343c40d
liyusang1 <liyusang1@naver.com>
2022-02-17 15:55:13 +0900

    REFACTOR : step4 코드 수정

--- app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
+++ app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
@@ -9,6 +9,8 @@ import android.util.TypedValue
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
@@ -18,7 +20,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.d("MainActivity", "onCreate")
-
         val name = "Andrew"
         val mainText = findViewById<TextView>(R.id.textView)
         mainText.text = "${name}의 사진 액자"
@@ -26,23 +27,31 @@ class MainActivity : AppCompatActivity() {
         mainText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30f)
         mainText.setBackgroundColor(Color.YELLOW)
 
-    }
+        val layout: ConstraintLayout = findViewById(R.id.layout)
+        val getResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == RESULT_OK) {
+                    val message = it.data?.getStringExtra("message").toString()
+                    Snackbar.make(layout, message, Snackbar.LENGTH_SHORT).show()
+                }
+            }
 
-    fun viewMessage(view: View) {
-        Snackbar.make(view, "사진을 불러옵니다.", Snackbar.LENGTH_LONG)
-            .show();
+        val button1 = findViewById<TextView>(R.id.button1)
+        button1.setOnClickListener{
+            Snackbar.make(it, "사진을 불러옵니다.", Snackbar.LENGTH_LONG)
+                .show();
+            changeButtonText()
 
-        val text2 = findViewById<TextView>(R.id.button1)
-        text2.text = "다음"
-        setNextButton()
-    }
-
-    fun setNextButton() {
-        val button: Button = findViewById(R.id.button1)
-        button.setOnClickListener {
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
+            button1.setOnClickListener {
+                val intent = Intent(this, MainActivity2::class.java)
+                getResult.launch(intent)
+            }
         }
+    }
+
+    fun changeButtonText() {
+        val buttonText = findViewById<TextView>(R.id.button1)
+        buttonText.text = "다음"
     }
 
     override fun onStart() {
@@ -62,11 +71,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStop() {
         super.onStop()
-
         Log.d("MainActivity", "onStop")
-        val parentLayout = findViewById<View>(android.R.id.content)
-        Snackbar.make(parentLayout, "사진을 불러 왔습니다!!", Snackbar.LENGTH_LONG)
-            .show();
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity2.kt
+++ app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity2.kt
@@ -1,19 +1,25 @@
 package com.codesquad.kotlinphotoframe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
         Log.d("MainActivity2", "onStart")
-    }
 
-    fun goBefore(view: View) {
-        finish()
+        val button : Button = findViewById(R.id.button)
+        button.setOnClickListener{
+            val intent = Intent()
+            intent.putExtra("message", "사진을 불러왔습니다!!!")
+            setResult(RESULT_OK, intent)
+            finish()
+        }
     }
 
     override fun onStart() {
