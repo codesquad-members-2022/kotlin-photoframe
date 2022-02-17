a223c75a3b905a66027e3f84564d3c7e85aa0b02
hwj <jminie@naver.com>
2022-02-17 18:57:20 +0900

    feat : findById -> viewBinding 으로 구조 변경

--- app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
+++ app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.codesquad.kotlinphotoframe
 
-
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Color
@@ -8,30 +7,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.TypedValue
 import android.view.View
-import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import java.lang.Exception
 import kotlin.random.Random
 import android.content.Intent
+import com.codesquad.kotlinphotoframe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val myTextView = findViewById<TextView>(R.id.main_text)
-        myTextView.text = "Stark의 사진액자"
-        myTextView.setTextColor(Color.BLUE)
-        myTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30f)
+        binding.mainText.text = "Stark의 사진액자"
+        binding.mainText.setTextColor(Color.BLUE)
+        binding.mainText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30f)
 
-        val button = findViewById<Button>(R.id.main_button)
-        button.text = "사진 추가"
-        button.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22f)
-        button.setOnClickListener(this)
+        binding.mainButton.text = "사진 추가"
+        binding.mainButton.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22f)
+        binding.mainButton.setOnClickListener(this)
 
-        val buttonGoSubActivity = findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.button_go_sub_activity)
-        buttonGoSubActivity.setOnClickListener {
+        binding.buttonGoSubActivity.setOnClickListener {
             val intent = Intent(this, SubActivity::class.java)
             startActivity(intent)
         }
@@ -41,10 +40,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         when (v?.id) {
             R.id.main_button -> {
                 val imageView = findViewById<ImageView>(R.id.main_image)
-                val images = listOf( // Int 로 숫자를 랜덤 생성할 시 01, 02 와 같이 앞에 0을 구현하기 힘들어서 String 리스트로 처리
-                    "01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
-                    "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22"
-                )
+                val images =
+                    listOf( // Int 로 숫자를 랜덤 생성할 시 01, 02 와 같이 앞에 0을 구현하기 힘들어서 String 리스트로 처리
+                        "01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
+                        "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22"
+                    )
                 val randomIdx = Random.nextInt(images.size)
                 val image = images[randomIdx]
                 try {
--- app/src/main/java/com/codesquad/kotlinphotoframe/SubActivity.kt
+++ app/src/main/java/com/codesquad/kotlinphotoframe/SubActivity.kt
@@ -1,26 +1,36 @@
 package com.codesquad.kotlinphotoframe
 
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.TypedValue
 import android.widget.Button
 import android.widget.ImageView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import com.codesquad.kotlinphotoframe.databinding.ActivityMainBinding
+import com.codesquad.kotlinphotoframe.databinding.ActivitySubBinding
 
 class SubActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySubBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
+        binding = ActivitySubBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val subButton = findViewById<Button>(R.id.sub_button)
-        subButton.text = "선택"
-        subButton.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22f)
+        binding.subButton.text = "선택"
+        binding.subButton.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22f)
 
-        val subImage = findViewById<ImageView>(R.id.sub_image)
         val subAssetManager = resources.assets
         val subInputStream = subAssetManager.open("photoframe-border.jpg")
         val subBitmap: Bitmap = BitmapFactory.decodeStream(subInputStream)
-        subImage.setImageBitmap(subBitmap)
+        binding.subImage.setImageBitmap(subBitmap)
+
     }
+
 }
