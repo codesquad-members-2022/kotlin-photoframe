2c40ca3003f54f90c7491450449a5685531b5917
liyusang1 <liyusang1@naver.com>
2022-02-17 18:25:28 +0900

    FEAT : step 5 구현

--- app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
+++ app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.codesquad.kotlinphotoframe
 
-import android.content.Intent
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.util.TypedValue
-import android.view.View
-import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.snackbar.Snackbar
+import java.io.BufferedInputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,34 +22,23 @@ class MainActivity : AppCompatActivity() {
         Log.d("MainActivity", "onCreate")
         val name = "Andrew"
         val mainText = findViewById<TextView>(R.id.textView)
+        val imageView = findViewById<ImageView>(R.id.imageView)
         mainText.text = "${name}의 사진 액자"
-        mainText.setTextColor(Color.MAGENTA)
-        mainText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30f)
-        mainText.setBackgroundColor(Color.YELLOW)
-
-        val layout: ConstraintLayout = findViewById(R.id.layout)
-        val getResult =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-                if (it.resultCode == RESULT_OK) {
-                    val message = it.data?.getStringExtra("message").toString()
-                    Snackbar.make(layout, message, Snackbar.LENGTH_SHORT).show()
-                }
-            }
 
         val button1 = findViewById<TextView>(R.id.button1)
         button1.setOnClickListener{
             Snackbar.make(it, "사진을 불러옵니다.", Snackbar.LENGTH_LONG)
                 .show();
             changeButtonText()
-
-            button1.setOnClickListener {
-                val intent = Intent(this, MainActivity2::class.java)
-                getResult.launch(intent)
-            }
+            imageView.setImageBitmap(BitmapFactory.decodeStream(BufferedInputStream(resources.assets.open("${getRandomNumber()}.jpg"))))
         }
     }
 
-    fun changeButtonText() {
+    private fun getRandomNumber(): Int {
+        return (1..22).random()
+    }
+
+    private fun changeButtonText() {
         val buttonText = findViewById<TextView>(R.id.button1)
         buttonText.text = "다음"
     }
