d434ebb7aee40efe8e8b17642739e623ab08651c
ese111 <feung94@gmail.com>
2022-02-18 14:16:22 +0900

    feat: 메세지 출력 추가
    토스트 메세지 출력 구현

--- app/src/main/java/com/codesquad/kotlinphotoframe/SecondActivity.kt
+++ app/src/main/java/com/codesquad/kotlinphotoframe/SecondActivity.kt
@@ -6,8 +6,10 @@ import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
@@ -17,6 +19,7 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        val view: View = findViewById(R.id.back)
         activityName = this.localClassName
         val callbackName = "onCreate"
         Log.d("$activityName", "$callbackName")
@@ -34,8 +37,8 @@ class SecondActivity : AppCompatActivity() {
 //            intent.putExtra("msg", "사진을 가져오는 중"
 //            startActivity(intent)
             getContent.launch("image/*")
-            }
         }
+    }
 
     private fun imageCtrl(imageView: ImageView) = registerForActivityResult(ActivityResultContracts.GetContent()) {
         bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
@@ -44,6 +47,7 @@ class SecondActivity : AppCompatActivity() {
             MediaStore.Images.Media.getBitmap(contentResolver, it)
         }
         imageView?.setImageBitmap(bitmap)
+        Toast.makeText(applicationContext,"사진을 불러왔습니다.", Toast.LENGTH_SHORT).show()
     }
 
     override fun onRestart() {
